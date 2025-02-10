2e760b38e6dfdf46facdee2d262661289b75f82a
KappaNone <121200875+KappaNone@users.noreply.github.com>
2025-02-10 14:18:33 +0100

    added calibrate() function

--- ObjectAvoidance/ObjectAvoidance.ino
+++ ObjectAvoidance/ObjectAvoidance.ino
@@ -5,11 +5,13 @@
 #define MOTORB_PIN1 11  // Motor B control pin 1 (PWM)
 #define MOTORB_PIN2 10 // Motor B control pin 2 (PWM)
 
-
 // Speed control (0-255)
 const int _motorSpeed = 255;  // Maximum speed
 const int _turnSpeed = 200;   // Speed during turns
 const int _timeToTurnRight = 1000;
+
+const int _a_offset = 0; // speed offset for motor A
+const int _b_offset = 1; // speed offset for motor B
 
 float _duration_us, _distance_cm;
 
@@ -27,9 +29,11 @@ void setup() {
 }
 
 void forward() {
-  analogWrite(MOTORA_PIN1, _motorSpeed);
+  int a_speed = calibrate(_motorSpeed, _a_offset);
+  int b_speed = calibrate(_motorSpeed, _b_offset);
+  analogWrite(MOTORA_PIN1, a_speed);
   analogWrite(MOTORA_PIN2, 0);
-  analogWrite(MOTORB_PIN1, _motorSpeed);
+  analogWrite(MOTORB_PIN1, b_speed);
   analogWrite(MOTORB_PIN2, 0);
 }
 
@@ -60,8 +64,15 @@ void loop() {
   // calculate the distance
   _distance_cm = 0.017 * _duration_us; // 343 * 100 * (1/2) * (1/1000000) = 0.017
 
-  if (_distance_cm <= 15) {
+  if (_distance_cm <= 15 && _distance_cm != 0) {
     turnRight();
     delay(_timeToTurnRight);
   }
 }
+
+
+int calibrate(int n, int offset) {
+  int result = n - offset;
+  if (offset < 0) result = 0;
+  return result;
+};
